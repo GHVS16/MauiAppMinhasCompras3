07b998446605869fe16a14197c628a8b98b57797
GHVS16 <guustavo667@gmail.com>
2025-03-29 21:29:43 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras3/Views/EditarProduto.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAppMinhasCompras3.Views;
+
+public partial class EditarProduto : ContentPage
+{
+	public EditarProduto()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiAppMinhasCompras3/Views/NovoProduto.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAppMinhasCompras3.Views;
+
+public partial class NovoProduto : ContentPage
+{
+	public NovoProduto()
+	{
+		InitializeComponent();
+	}
+}
