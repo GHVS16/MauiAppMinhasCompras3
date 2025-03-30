a35c64f765619853b44a40ba44df81bbd64bfcf2
GHVS16 <guustavo667@gmail.com>
2025-03-30 20:48:45 -0300

    Construção da classe para fazer CRUD no SQLite

--- MauiAppMinhasCompras3/Helpers/SQLiteDatabaseHelper.cs
+++ MauiAppMinhasCompras3/Helpers/SQLiteDatabaseHelper.cs
@@ -38,7 +38,7 @@ namespace MauiAppMinhasCompras3.Helpers
         
         public Task<List<Produto>> Search(string q)
         {
-            string sql = "SELECT *Produto WHERE descricao LIKE '%" + q + "%"; 
+            string sql = "SELECT *Produto WHERE descricao LIKE '%" + q + "%";
             return _conn.QueryAsync<Produto>(sql);
 
 
